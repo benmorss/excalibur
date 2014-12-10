50502a920b8cd661f86545c8907d488c80c951e9
Trevor Narayan <tnarayan@gmail.com>
2014-12-10 09:49:56 -0500

    Updateds.

--- static/js/new_excalibur.js
+++ static/js/new_excalibur.js
@@ -1,12 +1,12 @@
 function loadStaticData(input) {
-    $.get('/files/' + input, function(csv) {
+//    $.get('/files/' + input, function(csv) {
+    $.get('/static/result.csv', function(csv) {
         visualize(csv);
     });
 }
 
 function visualize(csv) {
     renderable = d3.csv.parse(csv)
-    console.log(renderable)
     var chart = createLineViewForData(renderable);
     chart.draw(renderable);
 }
@@ -17,8 +17,18 @@ function createTableViewForData(data) {
 }
 
 function createLineViewForData(data) {
-  var chart = new aplos.chart.LineChartView();
-  chart.configureSeries().x('week_start_date');
-  chart.configureSeries().y('queries');
-  return chart
+  if (data == null || data.length === 0) { // null input, return empty chart
+    return new aplos.chart.LineChartView();
+  } else if (Object.keys(data[0]).length === 2) { // single series line view
+    var chart = new aplos.chart.LineChartView().frameSize({width: 800, height: 300});
+    var keys = Object.keys(data[0]);
+    chart.configureSeries().x(keys[0]);
+    chart.configureSeries().y(keys[1]);
+    return chart;
+  } else {
+    var chart = new aplos.chart.LineChartView().frameSize({width: 800, height: 300});
+    var keys = Object.keys(data[0]);
+    var x_axis = keys[0];
+    return new aplos.chart.LineChartView();
+  }
 }
