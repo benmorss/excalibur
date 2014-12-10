56b92fd6707aa9cfc1d9fa67fc3e0de1f242e40f
Trevor Narayan <tnarayan@gmail.com>
2014-12-09 19:09:13 -0500

    Generalized new_excalibur.js

--- static/js/new_excalibur.js
+++ static/js/new_excalibur.js
@@ -7,8 +7,18 @@ function loadStaticData(input) {
 function visualize(csv) {
     renderable = d3.csv.parse(csv)
     console.log(renderable)
-    var chart = new aplos.chart.LineChartView();
-    chart.configureSeries().x('week_start_date');
-    chart.configureSeries().y('queries');
+    var chart = createLineViewForData(renderable);
     chart.draw(renderable);
+}
+
+function createTableViewForData(data) {
+  var chart = new aplos.chart.LineChartView();
+  return chart;
+}
+
+function createLineViewForData(data) {
+  var chart = new aplos.chart.LineChartView();
+  chart.configureSeries().x('week_start_date');
+  chart.configureSeries().y('queries');
+  return chart
 }
